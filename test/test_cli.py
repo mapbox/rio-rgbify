d0141d906603bf914099381eed54898c46c52452
dnomadb <damon@mapbox.com>
2016-08-03 14:05:12 -0700

    using flex testing for weird version handling

--- test/test_cli.py
+++ test/test_cli.py
@@ -2,10 +2,13 @@ from click.testing import CliRunner
 import rasterio as rio
 import numpy as np
 from rio_rgbify.scripts.cli import rgbify
+import click
 
 from tempfile import mkdtemp
 from shutil import rmtree
 import os
+
+from raster_tester.compare import affaux, upsample_array
 
 class TestingDir:
     def __init__(self):
@@ -16,6 +19,20 @@ class TestingDir:
         rmtree(self.tmpdir)
     def mkpath(self, filename):
         return os.path.join(self.tmpdir, filename)
+
+def flex_compare(r1, r2, thresh=10):
+    upsample = 4
+    r1 = r1[::upsample]
+    r2 = r2[::upsample]
+    toAff, frAff = affaux(upsample)
+    r1 = upsample_array(r1, upsample, frAff, toAff)
+    r2 = upsample_array(r2, upsample, frAff, toAff)
+    tdiff = np.abs(r1.astype(np.float64) - r2.astype(np.float64))
+
+    click.echo('{0} values exceed the threshold difference with a max variance of {1}'.format(
+        np.sum(tdiff > thresh), tdiff.max()), err=True)
+
+    return not np.any(tdiff > thresh)
 
 
 def test_cli_good_elev():
@@ -31,7 +48,10 @@ def test_cli_good_elev():
 
         with rio.open(out_rgb_src) as created:
             with rio.open(expected_src) as expected:
-                assert np.array_equal(created.read(), expected.read())
+                carr = created.read()
+                earr = expected.read()
+                for a, b in zip(carr, earr):
+                    assert flex_compare(a, b)
 
 
 def test_cli_fail_elev():
