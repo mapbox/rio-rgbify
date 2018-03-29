820129b0b122b64a07e325f63969a166acf8331c
vincentsarago <vincent.sarago@mapbox.com>
2018-03-29 10:02:06 -0400

    use  runner.isolated_filesystem instead of TestingDir

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,24 +1,19 @@
-from click.testing import CliRunner
-import rasterio as rio
-import numpy as np
-from rio_rgbify.scripts.cli import rgbify
-import click
-
-from tempfile import mkdtemp
-from shutil import rmtree
 import os
+
+import click
+from click.testing import CliRunner
+
+import numpy as np
+
+import rasterio as rio
+from rio_rgbify.scripts.cli import rgbify
 
 from raster_tester.compare import affaux, upsample_array
 
-class TestingDir:
-    def __init__(self):
-        self.tmpdir = mkdtemp()
-    def __enter__(self):
-        return self
-    def __exit__(self, a, b, c):
-        rmtree(self.tmpdir)
-    def mkpath(self, filename):
-        return os.path.join(self.tmpdir, filename)
+
+in_elev_src = os.path.join(os.path.dirname(__file__), 'fixtures', 'elev.tif')
+expected_src = os.path.join(os.path.dirname(__file__), 'expected', 'elev-rgb.tif')
+
 
 def flex_compare(r1, r2, thresh=10):
     upsample = 4
@@ -36,17 +31,13 @@ def flex_compare(r1, r2, thresh=10):
 
 
 def test_cli_good_elev():
-    in_elev_src = 'test/fixtures/elev.tif'
-    expected_src = 'test/expected/elev-rgb.tif'
-    with TestingDir() as tmpdir:
-        out_rgb_src = tmpdir.mkpath('rgb.tif')
-
-        runner = CliRunner()
-        result = runner.invoke(rgbify, [in_elev_src, out_rgb_src, '--interval', 0.001, '--base-val', -100, '-j', 1])
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(rgbify, [in_elev_src, 'rgb.tif', '--interval', 0.001, '--base-val', -100, '-j', 1])
 
         assert result.exit_code == 0
 
-        with rio.open(out_rgb_src) as created:
+        with rio.open('rgb.tif') as created:
             with rio.open(expected_src) as expected:
                 carr = created.read()
                 earr = expected.read()
@@ -55,117 +46,91 @@ def test_cli_good_elev():
 
 
 def test_cli_fail_elev():
-    in_elev_src = 'test/fixtures/elev.tif'
-    expected_src = 'test/expected/elev-rgb.tif'
-    with TestingDir() as tmpdir:
-        out_rgb_src = tmpdir.mkpath('rgb.tif')
-
-        runner = CliRunner()
-        result = runner.invoke(rgbify, [in_elev_src, out_rgb_src, '--interval', 0.00000001, '--base-val', -100, '-j', 1])
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        result = runner.invoke(rgbify, [in_elev_src, 'rgb.tif', '--interval', 0.00000001, '--base-val', -100, '-j', 1])
         assert result.exit_code == -1
 
 
 def test_mbtiler_webp():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles_finer = tmpdir.mkpath('output-0-dot-1.mbtiles')
-        runner = CliRunner()
-
-        result_finer = runner.invoke(rgbify, [in_elev_src, out_mbtiles_finer, '--interval', 0.1, '--min-z', 10, '--max-z', 11, '--format', 'webp', '-j', 1])
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles_finer = 'output-0-dot-1.mbtiles'
+        result_finer = runner.invoke(rgbify, [in_elev_src, out_mbtiles_finer,
+                                              '--interval', 0.1, '--min-z', 10, '--max-z', 11,
+                                              '--format', 'webp', '-j', 1])
         assert result_finer.exit_code == 0
 
-        out_mbtiles_coarser  = tmpdir.mkpath('output-1.mbtiles')
-
-        result_coarser = runner.invoke(rgbify, [in_elev_src, out_mbtiles_coarser, '--min-z', 10, '--max-z', 11, '--format', 'webp', '-j', 1])
-
+        out_mbtiles_coarser = 'output-1.mbtiles'
+        result_coarser = runner.invoke(rgbify, [in_elev_src, out_mbtiles_coarser,
+                                                '--min-z', 10, '--max-z', 11, '--format', 'webp', '-j', 1])
         assert result_coarser.exit_code == 0
 
         assert os.path.getsize(out_mbtiles_finer) > os.path.getsize(out_mbtiles_coarser)
 
 
 def test_mbtiler_png():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles_finer = tmpdir.mkpath('output-0-dot-1.mbtiles')
-        runner = CliRunner()
-
-        result_finer = runner.invoke(rgbify, [in_elev_src, out_mbtiles_finer, '--interval', 0.1, '--min-z', 10, '--max-z', 11, '--format', 'png'])
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles_finer = 'output-0-dot-1.mbtiles'
+        result_finer = runner.invoke(rgbify, [in_elev_src, out_mbtiles_finer,
+                                              '--interval', 0.1, '--min-z', 10, '--max-z', 11, '--format', 'png'])
         assert result_finer.exit_code == 0
 
-        out_mbtiles_coarser  = tmpdir.mkpath('output-1.mbtiles')
-
-        result_coarser = runner.invoke(rgbify, [in_elev_src, out_mbtiles_coarser, '--min-z', 10, '--max-z', 11, '--format', 'png', '-j', 1])
-
+        out_mbtiles_coarser = 'output-1.mbtiles'
+        result_coarser = runner.invoke(rgbify, [in_elev_src, out_mbtiles_coarser,
+                                                '--min-z', 10, '--max-z', 11, '--format', 'png', '-j', 1])
         assert result_coarser.exit_code == 0
 
         assert os.path.getsize(out_mbtiles_finer) > os.path.getsize(out_mbtiles_coarser)
 
+
 def test_mbtiler_png_bounding_tile():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles_not_limited = tmpdir.mkpath('output-not-limited.mbtiles')
-        runner = CliRunner()
-
-        result_not_limited = runner.invoke(rgbify, [in_elev_src, out_mbtiles_not_limited, '--min-z', 12, '--max-z', 12, '--format', 'png'])
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles_not_limited = 'output-not-limited.mbtiles'
+        result_not_limited = runner.invoke(rgbify, [in_elev_src, out_mbtiles_not_limited,
+                                                    '--min-z', 12, '--max-z', 12, '--format', 'png'])
         assert result_not_limited.exit_code == 0
 
-        out_mbtiles_limited  = tmpdir.mkpath('output-limited.mbtiles')
-
-        result_limited = runner.invoke(rgbify, [in_elev_src, out_mbtiles_limited, '--min-z', 12, '--max-z', 12, '--format', 'png', '--bounding-tile', '[654, 1582, 12]'])
-
+        out_mbtiles_limited = 'output-limited.mbtiles'
+        result_limited = runner.invoke(rgbify, [in_elev_src, out_mbtiles_limited,
+                                                '--min-z', 12, '--max-z', 12, '--format', 'png',
+                                                '--bounding-tile', '[654, 1582, 12]'])
         assert result_limited.exit_code == 0
 
         assert os.path.getsize(out_mbtiles_not_limited) > os.path.getsize(out_mbtiles_limited)
 
 
 def test_mbtiler_webp_badzoom():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles = tmpdir.mkpath('output.mbtiles')
-        runner = CliRunner()
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles = 'output.mbtiles'
         result = runner.invoke(rgbify, [in_elev_src, out_mbtiles, '--min-z', 10, '--max-z', 9, '--format', 'webp', '-j', 1])
-
         assert result.exit_code == -1
+
 
 def test_mbtiler_webp_badboundingtile():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles = tmpdir.mkpath('output.mbtiles')
-        runner = CliRunner()
-
-        result = runner.invoke(rgbify, [in_elev_src, out_mbtiles, '--min-z', 10, '--max-z', 9, '--format', 'webp', '--bounding-tile', '654, 1582, 12'])
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles = 'output.mbtiles'
+        result = runner.invoke(rgbify, [in_elev_src, out_mbtiles,
+                                        '--min-z', 10, '--max-z', 9, '--format', 'webp', '--bounding-tile', '654, 1582, 12'])
         assert result.exit_code == -1
 
+
 def test_mbtiler_webp_badboundingtile_values():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles = tmpdir.mkpath('output.mbtiles')
-        runner = CliRunner()
-
-        result = runner.invoke(rgbify, [in_elev_src, out_mbtiles, '--min-z', 10, '--max-z', 9, '--format', 'webp', '--bounding-tile', '[654, 1582]'])
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles = 'output.mbtiles'
+        result = runner.invoke(rgbify, [in_elev_src, out_mbtiles,
+                                        '--min-z', 10, '--max-z', 9, '--format', 'webp', '--bounding-tile', '[654, 1582]'])
         assert result.exit_code == -1
 
 
 def test_bad_input_format():
-    in_elev_src = 'test/fixtures/elev.tif'
-
-    with TestingDir() as tmpdir:
-        out_mbtiles = tmpdir.mkpath('output.lol')
-        runner = CliRunner()
-
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        out_mbtiles = 'output.lol'
         result = runner.invoke(rgbify, [in_elev_src, out_mbtiles, '--min-z', 10, '--max-z', 9, '--format', 'webp', '-j', 1])
-
         assert result.exit_code == -1
